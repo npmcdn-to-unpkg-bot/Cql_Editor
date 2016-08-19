cf9e17179d6741ef07e7a928c6e66dd62207df42
JClingo <josh@databasecg.com>
2016-08-19 16:51:23 -0600

    Began adding api service

--- source/src/app/app.component.ts
+++ source/src/app/app.component.ts
@@ -1,47 +1,65 @@
 import { Component } from '@angular/core';
 import { AceEditorDirective } from 'ng2-ace-editor';
 //import { MdButton } from '@angular2-material/button';
+import { APIService } from './shared/index';
 
 import 'brace/theme/clouds';
 import 'brace/mode/sql';
 import 'cql-ace-syntax/cql'; 
 
-
 @Component({
   moduleId: module.id,
   directives: [AceEditorDirective],
+  providers: [APIService],
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
 
-  loading: boolean = false;
+  constructor (private _apiService: APIService) {
+
+  }
+
+  error : string = '';
+  running: boolean = false;
 
   // Input editor settings
-  iText: string = `// Enter your CQL script here and press 'Run'"
-// The results will be displayed on the console to the right
+  iText: string = `# Enter your CQL script here and press 'Run'"
+# The results will be displayed on the console to the right
 
 `;
   iOptions:any = {vScrollBarAlwaysVisible: true} ;
-  
+  iTheme: string = "clouds";
+
   runScript() {
     // TODO: Add call to API service
-    this.loading = true;
-    this.oText += this.iText;
+    if (!this.running) {
+      this.running = true;
+      this._apiService
+        .post(this.iText)
+        .then(response => this.oText = response)
+        .catch(error => this.error = error);
+      this.oText += this.iText;
+    }
+    
     //this.loading = false;
   }
-  
+
+  onInputChange(code) {
+    this.iText = code;
+  }
+
 
   // Output editor settings
-  oTheme: string = "clouds";
-  oText: string = `// This is where your CQL script results will be displayed
+  
+  oText: string = `# This is where your CQL script results will be displayed
 
 `;
   oOptions:any = { vScrollBarAlwaysVisible: true, showLineNumbers: false , showGutter: false };
   oIsReadOnly: boolean = true;
 
-  clearConsole() {
+  clearOutput() {
     this.oText = '';
   }
 
---
+++ source/src/app/shared/api/api.service.ts
@@ -0,0 +1,61 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class APIService {
+
+  private serviceUrl = 'https://google.com';  // URL to web api
+
+  constructor(private http: Http) { }
+//   getHeroes() {
+//     return this.http.get(this.heroesUrl)
+//                .toPromise()
+//                .then(response => response.json().data as Hero[])
+//                .catch(this.handleError);
+//   }
+//   getHero(id: number) {
+//     return this.getHeroes()
+//                .then(heroes => heroes.find(hero => hero.id === id));
+//   }
+//   save(hero: Hero): Promise<Hero>  {
+//     if (hero.id) {
+//       return this.put(hero);
+//     }
+//     return this.post(hero);
+//   }
+//   delete(hero: Hero) {
+//     let headers = new Headers();
+//     headers.append('Content-Type', 'application/json');
+//     let url = `${this.heroesUrl}/${hero.id}`;
+//     return this.http
+//                .delete(url, {headers: headers})
+//                .toPromise()
+//                .catch(this.handleError);
+//   }
+  // Send code statement
+  post(code: string): Promise<string> {
+    let headers = new Headers({
+      'Content-Type': 'application/json'});
+    return this.http
+               .post(this.serviceUrl, code, {headers: headers})
+               .toPromise()
+               .then(res => res.json().data)
+               .catch(this.handleError);
+  }
+  // Update existing Hero
+//   private put(hero: Hero) {
+//     let headers = new Headers();
+//     headers.append('Content-Type', 'application/json');
+//     let url = `${this.heroesUrl}/${hero.id}`;
+//     return this.http
+//                .put(url, JSON.stringify(hero), {headers: headers})
+//                .toPromise()
+//                .then(() => hero)
+//                .catch(this.handleError);
+//   }
+  private handleError(error: any) {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+}
