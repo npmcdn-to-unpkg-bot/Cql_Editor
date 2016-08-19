2e516e551727b45c5b27597866e07918784e674f
JClingo <josh@databasecg.com>
2016-08-19 15:04:17 -0600

    Styled and customized

--- source/src/app/app.component.ts
+++ source/src/app/app.component.ts
@@ -16,10 +16,42 @@ import 'cql-ace-syntax/cql';
 })
 export class AppComponent {
 
-  iText: string = "";
-  iOptions:any = 
-    {vScrollBarAlwaysVisible: true}
-  ;
+  loading: boolean = false;
+
+  // Input editor settings
+  iText: string = `// Enter your CQL script here and press 'Run'"
+// The results will be displayed on the console to the right
+
+`;
+  iOptions:any = {vScrollBarAlwaysVisible: true} ;
+  
+  runScript() {
+    // TODO: Add call to API service
+    this.loading = true;
+    this.oText += this.iText;
+    //this.loading = false;
+  }
+  
+
+  // Output editor settings
+  oTheme: string = "clouds";
+  oText: string = `// This is where your CQL script results will be displayed
+
+`;
+  oOptions:any = { vScrollBarAlwaysVisible: true, showLineNumbers: false , showGutter: false };
+  oIsReadOnly: boolean = true;
+
+  clearConsole() {
+    this.oText = '';
+  }
+
+  resources: boolean = false;
+
+  toggleResources () {
+    this.resources = !this.resources;
+  }
+
+  // TODO: Strip comments from user's script
 
 
 }
